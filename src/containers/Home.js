fcd115a722c0f84716f589f4a01aaaf1e0082f94
min-hieu <hieuristics@kaist.ac.kr>
2021-11-05 13:01:31 +0900

    added tiles to homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import { PRIMARY_COLOR } from '../Constant';
 import Btn from '../components/shared/button';
 import Navbar from '../components/shared/Navbar';
+import testImg from '../assets/test/test_img.png';
 
 const styles = {
 	main: {
 		background: 'white',
 		borderRadius: 20,
 		height: '100%',
+		overflow: 'scroll',
+		marginBottom: 20,
 	},
 	media: {
     height: 300,
@@ -26,49 +32,61 @@ const styles = {
 	sessionHeading: {
 		fontSize: 20,
 		color: PRIMARY_COLOR,
-		padding: 16,
+		paddingLeft: 16,
+		padding: 10,
 	},
-	listWrapper: {
-		width: '100%',
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
+	tileTitle: {
+		height: '100%',
+    top: '50%',
+    transform: 'translate(0, -50%)',
+    textAlign: 'center',
+		backgroundColor: '#00000036',
+		'-webkit-user-select': 'none', /* Safari */        
+		'-moz-user-select': 'none', /* Firefox */
+		'-ms-user-select': 'none', /* IE10+/Edge */
+		'user-select': 'none', /* Standard */
 	},
-	testCard: {
-		padding: 16,
+	tileRoot: {
+		marginLeft: 15,
 	},
+	tile: {
+		borderRadius: 10,
+	},
+	gridListWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+		overflow: 'hidden',
+		'-webkit-scrollbar-display': 'None',
+  },
+  gridList: {
+    flexWrap: 'nowrap'
+  },
 };
 
 function Home({ classes }) {
-	const sessionList = [
-		<Card className={classes.testCard}>
-			<CardActionArea>
-				<CardMedia
-					className={classes.mediaList}
-					image={require('../assets/test/test_img.png')}
-				/>
-			</CardActionArea>
-		</Card>,
-		<Card className={classes.testCard}>
-			<CardActionArea>
-				<CardMedia
-					className={classes.mediaList}
-					image={require('../assets/test/test_img.png')}
-				/>
-			</CardActionArea>
-		</Card>,
-		<Card className={classes.testCard}>
-			<CardActionArea>
-				<CardMedia
-					className={classes.mediaList}
-					image={require('../assets/test/test_img.png')}
-				/>
-			</CardActionArea>
-		</Card>,
-	];
+	const tileData = [
+		{
+			img: testImg,
+			title: 'this is fucked'
+		},	
+		{
+			img: testImg,
+			title: 'title'
+		},	
+		{
+			img: testImg,
+			title: 'title'
+		},	
+		{
+			img: testImg,
+			title: 'fuck'
+		},	
+	]
 
   return (
     <div className={classes.main}>
+
 			<Card>
 				<CardActionArea>
 					<CardMedia
@@ -77,12 +95,46 @@ function Home({ classes }) {
 					/>
 				</CardActionArea>
 			</Card>
+
 			<Typography className={classes.sessionHeading}>This week's top sessions</Typography>
-			<div className={classes.listWrapper}>
-				{sessionList.map(
-					session => session
-				)}
+
+			<div className={classes.gridListWrapper}>
+				<GridList className={classes.gridList} cols={2.5}>
+					{tileData.map((tile) => (
+						<GridListTile key={tile.img}
+							classes={{ root: classes.tileRoot, tile: classes.tile }}>
+							<img src={tile.img} alt={tile.title} />
+							<GridListTileBar className={classes.tileTitle}
+								title={tile.title}
+							/>
+						</GridListTile>
+					))}
+				</GridList>
 			</div>
+
+			<Typography className={classes.sessionHeading}>This week's top collab</Typography>
+
+			<div className={classes.gridListWrapper}>
+				<GridList className={classes.gridList} cols={2.5}>
+					{tileData.map((tile) => (
+						<GridListTile key={tile.img}
+							classes={{ root: classes.tileRoot, tile: classes.tile }}>
+							<img src={tile.img} alt={tile.title} />
+							<GridListTileBar className={classes.tileTitle}
+								title={tile.title}
+							/>
+						</GridListTile>
+					))}
+				</GridList>
+			</div>
+			
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+
 			<Navbar />
     </div>
   );
