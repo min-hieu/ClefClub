d105c3b0a72dc2fd1b21cb6c8cc51929b410bdb2
John <john_mai_2605@kaist.ac.kr>
2021-11-16 19:58:39 +0900

    size

--- src/components/shared/YoutubeEmbed.js
+++ src/components/shared/YoutubeEmbed.js
@@ -13,8 +13,6 @@ const styles = {
   videoResponsiveIframe: {
     left: 0,
     top: 0,
-    maxHeight: '100%',
-    maxWidth: '99.9%',
     position: 'absolute',
   },  
 }
@@ -27,7 +25,6 @@ const YoutubeEmbed = ({classes, embedId, w, h }) => (
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
       title="Embedded youtube"
     />
   </div>
--- src/containers/collab/preview/PreviewCollab.js
+++ src/containers/collab/preview/PreviewCollab.js
@@ -148,13 +148,13 @@ const CollabPreview = ({ classes }) => {
       <br/>
       {title}
       {subTitle1}
-      <YoutubeEmbed embedId="6mYw53V9RGM?autoplay=1" w="853" h="480" />
+      <YoutubeEmbed embedId="6mYw53V9RGM?autoplay=1" w="99%" h="100%" />
       {receive ? decision : null}
       {progressInstance}
       {receive 
         ? <>
             {subTitle2}
-            <YoutubeEmbed embedId="u5IEr6jMuHw"  w="853" h="480" ></YoutubeEmbed>
+            <YoutubeEmbed embedId="u5IEr6jMuHw"  w="99%" h="100%" ></YoutubeEmbed>
           </>
         : null
       }
