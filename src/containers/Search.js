c53c98a5fe623cb395ae2eb4fa095faeecf6b2da
Ngoc <ngocdb169@gmail.com>
2021-11-16 22:51:21 +0900

    search for john

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
 import CancelIcon from '@material-ui/icons/Cancel';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import Avatar from '@mui/material/Avatar';
+import { PRIMARY_COLOR, TERTIARY_COLOR } from '../Constant';
 import Navbar from '../components/shared/Navbar';
+import picture from '../assets/test/monkey.jpeg';
 
 const styles = {
   main: {
@@ -47,11 +53,42 @@ const styles = {
     width: '100%',
     fontSize: 15,
   },
-  searchUser: {},
   searchCollab: {
-    background: '#ffddd2',
-    width: 270,
-    height: 300,
+    maxWidth: 400,
+    margin: '10px auto',
+  },
+  searchUser: {
+    background: '#fff7f4',
+    maxWidth: 400,
+    height: 100,
+    margin: '10px auto',
+  },
+  media: {
+    height: 150,
+    borderRadius: 10,
+  },
+  searchCollabTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  searchCollabSubtitle: {
+    fontSize: 12,
+    color: PRIMARY_COLOR,
+  },
+  avatar: {
+    width: '70px !important',
+    height: '70px !important',
+  },
+  name: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: 1.5,
+  },
+  collabs: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: PRIMARY_COLOR,
   },
 };
 
@@ -109,16 +146,34 @@ function Search({ classes }) {
             container
             direction="column"
             justifyContent="center"
-            className={classes.searchUser}
+            className={classes.searchCollab}
           >
+            <Card>
+              <CardActionArea>
+                <CardMedia className={classes.media} image={require('../assets/test/test_img.png')}/>
+              </CardActionArea>
+            </Card>
+            <Typography className={classes.searchCollabTitle}>Carpe DiJohn</Typography>
+            <Typography className={classes.searchCollabSubtitle}>John Wahjosky · 69K views · 6 days ago</Typography>
           </Grid>
           <Grid
             item
             container
-            direction="column"
+            className={classes.searchUser}
+            alignItems="center"
             justifyContent="center"
-            className={classes.searchCollab}
           >
+            <Grid item xs={2}>
+              <Avatar className={classes.avatar} alt="John" src={ picture }/>
+            </Grid>
+            <Grid item xs={8} container direction="column" justifyContent="center">
+              <Grid item>
+                <Typography variant="h1" className={classes.name}>John</Typography>
+              </Grid>
+              <Grid item>
+                <Typography className={classes.collabs}>169 jams</Typography>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       )}
