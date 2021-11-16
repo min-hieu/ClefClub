135ebdcbb2ee93095e595da961101675cab03368
John <john_mai_2605@kaist.ac.kr>
2021-11-17 02:39:47 +0900

    Beautification

--- src/components/userAuth/ForgotPassword.js
+++ src/components/userAuth/ForgotPassword.js
@@ -1,14 +1,79 @@
 import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
+import { withStyles } from '@material-ui/core/styles';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@material-ui/core/Typography';
+import { PRIMARY_COLOR, SECONDARY_COLOR, TERTIARY_COLOR } from "../../Constant";
+import Alert from '@mui/material/Alert';
 import { useAuth } from "../../contexts/AuthContext"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
+import { db } from "../../firebase"
+import Navbar from "../shared/Navbar"
 
-export default function ForgotPassword() {
-  const emailRef = useRef()
+const styles = {
+  main: {
+    margin: '50px 35px 20px 35px',
+  },
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    '& .MuiTextField-root': {
+      width: '300px',
+    },
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  login: {
+    color: PRIMARY_COLOR
+  },
+  submitBtn: {
+    textTransform: 'none !important',
+    color: `black !important`,
+    backgroundColor: `${SECONDARY_COLOR} !important`,
+    '&:hover': {
+      backgroundColor: `${TERTIARY_COLOR} !important`,
+      color: `${PRIMARY_COLOR} !important`,
+    },
+  },
+}
+
+const FancyTextField = styled(TextField)({
+  '& label.MuiInputLabel-root': {
+    color: PRIMARY_COLOR,
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: PRIMARY_COLOR,
+  },
+  
+  '& .MuiOutlinedInput-root': {
+    backgroundColor: TERTIARY_COLOR,
+    '& fieldset': {
+      borderColor: TERTIARY_COLOR,
+      borderBottomColor: SECONDARY_COLOR,
+    },
+    '&:hover fieldset': {
+      borderColor: SECONDARY_COLOR,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: PRIMARY_COLOR,
+    },
+  },
+});
+
+function ForgotPassword({classes}) {
   const { resetPassword } = useAuth()
   const [error, setError] = useState("")
   const [message, setMessage] = useState("")
   const [loading, setLoading] = useState(false)
+
+  const [email, setEmail] = useState('');
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -17,18 +82,66 @@ export default function ForgotPassword() {
       setMessage("")
       setError("")
       setLoading(true)
-      await resetPassword(emailRef.current.value)
-      setMessage("Check your inbox for further instructions")
+      await resetPassword(email)
+      setMessage("Please check your email's inbox")
     } catch {
-      setError("Failed to reset password")
+      setError("Please type your email correctly")
     }
 
     setLoading(false)
   }
+  const title = <Typography className={classes.title}>Reset password</Typography>
+  const emailField =
+      <FancyTextField
+        label="Email"
+        variant="outlined"
+        type="email"
+        required
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+  const submitBtn = 
+    <Link to='/profile' style={{ textDecoration: 'none' }}>         
+      <Button variant="contained" className={classes.submitBtn} onClick = {handleSubmit}>
+        <Typography className={classes.btnText}> Reset password </Typography>
+      </Button>
+    </Link>
 
-  return (
-    <>
-      <Card>
+  const signup = 
+    <Typography className={classes.signup} >
+      Need an account? <Link to="/signup">Sign Up</Link>
+    </Typography>
+
+  const login = 
+    <Typography className={classes.signup} >
+      <Link to="/login">Log in </Link>
+    </Typography>
+
+    return (
+    <div className={classes.main}>
+      {title}
+      <Box
+        component="form"
+        sx={{'& > :not(style)': { m: 1 }}}
+        className={classes.root}
+      >
+        {emailField}
+        {submitBtn}
+        {error && <Alert severity="error">{error}</Alert>}
+        {message && <Alert severity="success">{message}</Alert>}
+        {login}
+        {signup}
+      </Box>
+      <Navbar />     
+    </div>
+  )
+}
+
+export default withStyles(styles)(ForgotPassword)
+
+  // return (
+    // <>
+      {/* <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Password Reset</h2>
           {error && <Alert variant="danger">{error}</Alert>}
@@ -50,6 +163,6 @@ export default function ForgotPassword() {
       <div className="w-100 text-center mt-2">
         Need an account? <Link to="/signup">Sign Up</Link>
       </div>
-    </>
-  )
-}
+    </> */}
+//   )
+// }
--- src/components/userAuth/Signup.js
+++ src/components/userAuth/Signup.js
@@ -1,80 +1,177 @@
 import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
+import { withStyles } from '@material-ui/core/styles';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@material-ui/core/Typography';
+import { PRIMARY_COLOR, SECONDARY_COLOR, TERTIARY_COLOR } from "../../Constant";
+import Alert from '@mui/material/Alert';
 import { useAuth } from "../../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import { db } from "../../firebase"
-import {createId} from "../../contexts/DBContext"
+import Navbar from "../shared/Navbar"
 
-export default function Signup() {
-  const nameRef = useRef()
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
+const styles = {
+  main: {
+    margin: '50px 35px 20px 35px',
+  },
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    '& .MuiTextField-root': {
+      width: '300px',
+    },
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  login: {
+    color: PRIMARY_COLOR
+  },
+  submitBtn: {
+    textTransform: 'none !important',
+    color: `black !important`,
+    backgroundColor: `${SECONDARY_COLOR} !important`,
+    '&:hover': {
+      backgroundColor: `${TERTIARY_COLOR} !important`,
+      color: `${PRIMARY_COLOR} !important`,
+    },
+  },
+}
+
+const FancyTextField = styled(TextField)({
+  '& label.MuiInputLabel-root': {
+    color: PRIMARY_COLOR,
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: PRIMARY_COLOR,
+  },
+  
+  '& .MuiOutlinedInput-root': {
+    backgroundColor: TERTIARY_COLOR,
+    '& fieldset': {
+      borderColor: TERTIARY_COLOR,
+      borderBottomColor: SECONDARY_COLOR,
+    },
+    '&:hover fieldset': {
+      borderColor: SECONDARY_COLOR,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: PRIMARY_COLOR,
+    },
+  },
+});
+
+function Signup({classes}) {
   const { signup } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
   const history = useHistory()
 
-
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [cfPassword, setcfPassword] = useState('');
 
   async function handleSubmit(e) {
-
-    db.collection("users").doc(emailRef.current.value).set({
-      nickname: nameRef.current.value,
-      email: emailRef.current.value,
-      sessions: [],
-    });
-
     e.preventDefault()
+    if (! (password && cfPassword && name && email)) {
+      return setError("Please enter all required fields")
+    }
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+    if (password !== cfPassword) {
       return setError("Passwords do not match")
     }
 
     try {
       setError("")
       setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
+      await signup(email, password)
       history.push("/")
-    } catch {
-      setError("Failed to create an account")
+      db.collection("users").doc(email).set({
+        nickname: name,
+        email: email,
+        sessions: [],
+      });
+    } 
+    catch {
+      setError("Please use the correct email and make a safe password")
     }
 
     setLoading(false)
   }
 
-  return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="name">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="name" ref={nameRef} required />
-            </Form.Group>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
-    </>
+  const title = <Typography className={classes.title}>Sign up</Typography>
+  const nameField =
+    <FancyTextField
+      label="First Name"
+      variant="outlined"
+      required
+      value={name}
+      onChange={e => setName(e.target.value)}
+    />;
+  const emailField =
+      <FancyTextField
+        label="Email"
+        variant="outlined"
+        type="email"
+        required
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+  const pwField1 = 
+    <FancyTextField
+      label="Password"
+      variant="outlined"
+      type="password"
+      required
+      value={password}
+      onChange={e => setPassword(e.target.value)}
+    />
+  const pwField2 = 
+    <FancyTextField
+      label="Confirm Password"
+      variant="outlined"
+      type="password"
+      required
+      value={cfPassword}
+      onChange={e => setcfPassword(e.target.value)}
+    />
+  const submitBtn = 
+    // <Link to='/profile' style={{ textDecoration: 'none' }}>         
+      <Button variant="contained" className={classes.submitBtn} onClick = {handleSubmit}>
+        <Typography className={classes.btnText}> Sign up </Typography>
+      </Button>
+    // </Link>
+  const login = 
+    <Typography className={classes.login} >
+      Already have an account? <Link to="/login">Log In</Link>
+    </Typography>
+
+    return (
+    <div className={classes.main}>
+      {title}
+      <Box
+        component="form"
+        sx={{'& > :not(style)': { m: 1 }}}
+        className={classes.root}
+      >
+        {nameField}
+        {emailField}
+        {pwField1}
+        {pwField2}
+        {submitBtn}
+        {error && <Alert severity="error">{error}</Alert>}
+        {login}
+      </Box>
+      <Navbar />     
+    </div>
   )
 }
+
+export default withStyles(styles)(Signup)
