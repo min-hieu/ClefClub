b97972408f570fcc098deb9c2dabafb2daeea854
John <john_mai_2605@kaist.ac.kr>
2021-11-16 15:06:16 +0900

    Add some logic

--- src/containers/collab/preview/PreviewCollab.js
+++ src/containers/collab/preview/PreviewCollab.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Navbar from '../../../components/shared/Navbar';
 import YoutubeEmbed from '../../../components/shared/YoutubeEmbed';
 import {Typography, Grid} from '@material-ui/core'
-import {PRIMARY_COLOR, SECONDARY_COLOR, TERTIARY_COLOR } from '../../../Constant';
+import {PRIMARY_COLOR, SECONDARY_COLOR, TERTIARY_COLOR, NEGATIVE_PRIMARY_COLOR, NEGATIVE_SECONDARY_COLOR} from '../../../Constant';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from '@mui/material/Button';
 import CheckIcon from '@mui/icons-material/Check';
@@ -12,16 +12,20 @@ import Stack from '@mui/material/Stack';
 import ClearIcon from '@mui/icons-material/Clear';
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
+const requester = 'ABMs';
+let approved = null;
+let final_approved = null;
+
 const styles = {
   title: {
     fontSize: 30,
     fontWeight: 'bold',
     margin: 10,
-    marginTop: 50,
   },
   subTitle: {
+    marginTop: '15px !important',
     fontSize: 15,
-    margin: 10,
+    margin: '30px 10px 10px 10px',
     color: PRIMARY_COLOR,
   },
   acceptButton: {
@@ -36,8 +40,8 @@ const styles = {
     textTransform: 'none !important',
     backgroundColor: '#ffdddd !important',
     '&:hover': {
-      backgroundColor: `#ffbbbb !important`,
-      color: '#ff2222',
+      backgroundColor: `${NEGATIVE_SECONDARY_COLOR} !important`,
+      color: NEGATIVE_PRIMARY_COLOR,
     },
   },
   decision: {
@@ -49,59 +53,89 @@ const styles = {
     marginLeft: 10,
     marginRight: 10,
   },
-
   caption: {
-    marginTop: '6px !important',
-    color: PRIMARY_COLOR,
+    marginTop: '0px !important',
+  },
+  approveText: {
+    color: PRIMARY_COLOR
+  },
+  declineText: {
+    color: NEGATIVE_PRIMARY_COLOR,
   }
+
 };
 
 const CollabPreview = ({ classes }) => {
-  const title =
+  const title = 
     <Grid container alignItems="center" justifyContent="center">
       <Grid item>
         <Typography className={classes.title}>CARPE DIEM</Typography>
       </Grid>
     </Grid>
 
-  const subTitle1 =
+  const subTitle1 = 
   <Grid container alignItems="center" justifyContent="center">
     <Grid item>
-      <Typography className={classes.subTitle}>Contribution requested by ABC</Typography>
+      <Typography className={classes.subTitle}>Contribution requested by {requester}</Typography>
     </Grid>
   </Grid>
 
-  const subTitle2 =
+  const subTitle2 = 
       <Grid container alignItems="center" justifyContent="center">
         <Grid item>
           <Typography className={classes.subTitle}>Still not sure? Let's see how it currently looks like</Typography>
         </Grid>
       </Grid>
 
-  const decision =
-    <Stack className = {classes.decision} direction="row" spacing={5} justifyContent="center">
-      <Link to='/notification' style={{ textDecoration: 'none' }}>
-        <Button className={classes.acceptButton} color="success" >
-            <CheckIcon />
-            <Typography className={classes.btnText}> Accept </Typography>
-        </Button>
-      </Link>
-      <Link to='/notification' style={{ textDecoration: 'none' }}>
-        <Button className={classes.declineButton} color="error" >
-            <ClearIcon />
-            <Typography className={classes.btnText}> Decline </Typography>
-        </Button>
-      </Link>
-    </Stack>
+  const handleApprove = () => {
+    approved = true;
+  }
+  const handleDecline = () => {
+    approved = false;
+  }
+  const approveButton = 
+    <Link to='/notification' style={{ textDecoration: 'none' }}>         
+      <Button className={classes.acceptButton} color="success" onClick={handleApprove}>
+          <CheckIcon />
+          <Typography className={classes.btnText}> Accept </Typography>
+      </Button>
+    </Link>
+  const declineButton =
+    <Link to='/notification' style={{ textDecoration: 'none' }}>         
+      <Button className={classes.declineButton} color="error" onClick = {handleDecline}>
+          <ClearIcon />
+          <Typography className={classes.btnText}> Decline </Typography>
+      </Button>
+    </Link>
 
-  const progressInstance =
+  const decision = 
+    approved === null
+    ? <Stack className = {classes.decision} direction="row" spacing={5} justifyContent="center">
+        {approveButton}
+        {declineButton}
+      </Stack>
+    : <Stack className = {classes.decision} direction="row" spacing={5} justifyContent="center">
+        <Typography variant="body1" className={approved ? classes.approveText : classes.declineText}> 
+          You have {approved ? 'approved' : 'declined'} this contribution 
+        </Typography>        
+      </Stack>
+
+  const progressInstance =   
       <Stack direction="column" spacing={5} alignItems="center">
-        <ProgressBar variant="success" now={40} style={{width: '90%'}} />
-        <Typography variant="caption" className={classes.caption}> Approved by 2/5 owner(s) </Typography>
+        <ProgressBar style={{width: '90%'}}>
+          <ProgressBar variant="success" now={40} key={1}/>
+          <ProgressBar variant="danger" now={20} key={2}/>
+        </ProgressBar>
+        <Stack direction="row" spacing={1} alignItems="center" className={classes.caption}>
+          <Typography variant="caption" className={classes.approveText}> Approved by 2/5 owner(s) </Typography>        
+          <Typography variant="caption"> - </Typography>        
+          <Typography variant="caption" className={classes.declineText}> Declined by 1/5 owner(s) </Typography> 
+        </Stack>       
       </Stack>;
 
   return (
     <>
+      <br/>
       {title}
       {subTitle1}
       <YoutubeEmbed embedId="6mYw53V9RGM?autoplay=1" />
